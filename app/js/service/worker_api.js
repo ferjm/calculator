3cbb7e664de244ccaf5725116f88ea9191d2bc25
Etienne Segonzac <etienne@segonzac.info>
2014-11-21 18:38:52 +0100

    Shall we update now?

--- app/js/service/worker_api.js
+++ app/js/service/worker_api.js
@@ -11,16 +11,16 @@ var implementation = {
     for (var filename in rv) {
       filesToUpdate++;
 
-      caches.match(filename).then((function(filename, response) {
-        caches.open('calculator-cache-v4').then((function(filename, cache) {
+      cachesAPI.match(filename).then((function(filename, response) {
+        //caches.open('calculator-cache-v4').then((function(filename, cache) {
 
           var originalUrl = response.url;
-          cache.delete(originalUrl).then(function onDeleted() {
+          cacheAPI.delete(originalUrl).then(function onDeleted() {
             var opts = {};
             opts['headers'] = { 'content-type': self._getContentType(filename) };
 
             var newResponse = new Response(rv[filename], opts);
-            cache.put(originalUrl, newResponse).then(function onSaved() {
+            cacheAPI.put(originalUrl, rv[filename]).then(function onSaved() {
               filesToUpdate--;
               if (filesToUpdate === 0) {
                 promise.resolve(true);
@@ -28,7 +28,7 @@ var implementation = {
             });
           });
         }).bind(this, filename));
-      }).bind(this, filename));
+      //}).bind(this, filename));
     }
 
     // There was nothing to update...
