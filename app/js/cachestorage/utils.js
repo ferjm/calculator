67d30087835d0609a464f0602370974256f8f0ec
Etienne Segonzac <etienne@segonzac.info>
2014-11-26 11:11:56 +0100

    Removing images and making patches with less context.

--- app/js/cachestorage/utils.js
+++ app/js/cachestorage/utils.js
@@ -8,23 +8,6 @@ function AppCache() {
     // style
     '/calculator/app/style/calculator.css',
 
-    // images
-    '/calculator/app/style/images/icon_plus.png',
-    '/calculator/app/style/images/icon_equal.png',
-    '/calculator/app/style/images/icon_divide.png',
-    '/calculator/app/style/images/icon_minus.png',
-    '/calculator/app/style/images/icon_multiply.png',
-    '/calculator/app/style/images/icon_plus@1.5x.png',
-    '/calculator/app/style/images/icon_equal@1.5x.png',
-    '/calculator/app/style/images/icon_divide@1.5x.png',
-    '/calculator/app/style/images/icon_minus@1.5x.png',
-    '/calculator/app/style/images/icon_multiply@1.5x.png',
-    '/calculator/app/style/images/icon_plus@2x.png',
-    '/calculator/app/style/images/icon_equal@2.png',
-    '/calculator/app/style/images/icon_divide@2.png',
-    '/calculator/app/style/images/icon_minus@2.png',
-    '/calculator/app/style/images/icon_multiply@2.png',
-
     // config
     '/calculator/app/config.json',
 
