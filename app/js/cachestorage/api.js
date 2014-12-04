78cedb2b2b06b4aa4f07fb0c9d77a2f3634475bd
Vivien Nicolas <vnicolas@mozilla.com>
2014-12-04 20:21:04 +0100

    Small nit to the cachestorage api.js side

--- app/js/cachestorage/api.js
+++ app/js/cachestorage/api.js
@@ -5,28 +5,23 @@ importScripts('/calculator/app/js/cachestorage/utils.js');
 importScripts('/calculator/app/js/protocols/protocol_helper.js');
 
 window.addEventListener('load', function() {
-
-  if (navigator.serviceWorker.controller) {
-    var protocol = new IPDLProtocol('cacheStorage');
-
-    protocol.recvMatch = function(resolve, reject, args) {
-      var key = args.key;
-      if (!key.startsWith('http')) {
-        key = location.protocol + '//' + location.host + key;
-      }
-
-      debug('recvMatch: ' + key);
-      asyncStorage.getItem(key, function(value) {
-        resolve(value);
-      });
-    }
-  }
-
-
   (new AppCache()).then(function onSuccess(shouldReload) {
     if (shouldReload) {
       new ServiceAPI(doSoftReload);
     } else {
+      var protocol = new IPDLProtocol('cacheStorage');
+
+      protocol.recvMatch = function(resolve, reject, args) {
+        var key = args.key;
+        if (!key.startsWith('http')) {
+          key = location.protocol + '//' + location.host + key;
+        }
+
+        asyncStorage.getItem(key, function(value) {
+          resolve(value);
+        });
+      }
+
       document.getElementById('content').src = 'index.html';
     }
   });
